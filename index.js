a9fe1317806581bb1f1ae1cb1c70b409300fcece
Anna Wilson <ajoyna01@gmail.com>
2021-06-16 20:03:21 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+  
+  
+ function findMatching(driversArray, string) {
+   const result = driversArray.filter(driver => string.toLowerCase() === driver.toLowerCase()) 
+  
+   return result
+}
+ 
+// expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
+
+function fuzzyMatch(driversFirstInitial, strings) {
+  const beforeEach = driversFirstInitial.filter(drivers => drivers.slice(0,2) === strings);
+  return beforeEach
+  }
+  
+  function matchName(driversMatch, name) {
+    return driversMatch.filter((driver) => driver.name === name)
+  }
+  
